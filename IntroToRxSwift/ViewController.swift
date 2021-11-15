edefbf38949cca1e78b48c420349805661a3bb65
Matthew Dolan <mdolan.code@gmail.com>
2021-11-14 18:08:28 -0800

    Setup the product model. Create productViewModel, publisher and item collection.

--- IntroToRxSwift/ViewController.swift
+++ IntroToRxSwift/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+struct Product {
+    let imageName: String
+    let title: String
+}
+
+struct ProductViewModel {
+    var items = PublishSubject<[Product]>()
+    
+    func fetchItems() {
+        let products = [
+            Product(imageName: "house", title: "Home"),
+            Product(imageName: "gear", title: "Settings"),
+            Product(imageName: "person.circle", title: "Profile"),
+            Product(imageName: "airplane", title: "Flights"),
+            Product(imageName: "bell", title: "Activity")
+        ]
+        
+        items.onNext(products)
+        items.onCompleted()
+    }
+}
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
